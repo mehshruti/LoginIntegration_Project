55b6cf99bdcb87a2525bb8e8132a8023206641b9
shruti mehta <140491471+mehshruti@users.noreply.github.com>
2024-05-05 23:33:19 +0530

    loginIntegration

---
+++ app/CreateUser/page.jsx
@@ -0,0 +1,12 @@
+// import UserForm from "../(components)/UserForm";
+import UserForm from "../(component)/UserForm";
+
+const CreateUser = () => {
+  return (
+    <div>
+      <UserForm />
+    </div>
+  );
+};
+
+export default CreateUser;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Nav from './(component)/Nav'
+import AuthProvider from './(component)/AuthProvider'
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +14,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html>
+      <AuthProvider>
+        <body className="bg-gray-100">
+          <Nav />
+          <div className="m-2">{children}</div>
+        </body>
+      </AuthProvider>
     </html>
   );
 }
